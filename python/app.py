b43bc4ecb5d3961b772d35c98d2803fb6edf9563
istamosh <63596969+istamosh@users.noreply.github.com>
2023-11-14 19:04:49 +0700

    vars and data types

--- python/app.py
+++ python/app.py
@@ -1,1 +1,18 @@
+# say hi to mom
 print("Hello mom!")
+# draw a triangle, determines the default order of python instruction from top to bottom
+print("    /|")
+print("   / |")
+print("  /  |")
+print(" /   |")
+print("/____|")
+# Using variables and data types, and overwriting vars
+char_name = "Tom"
+char_age = 27
+is_male = True
+print("There was a man named " +char_name+ ".")
+print("he was" ,char_age, "years old.")
+char_name = "Mike"
+char_age = 72
+print("He really liked the name " +char_name+ ".")
+print("but didn't like being" ,char_age, "years old.")
