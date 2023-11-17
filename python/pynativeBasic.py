4c3531f7d22273e843bb0961ebaca4e12f07d6a3
btiger-laptop <63596969+istamosh@users.noreply.github.com>
2023-11-17 16:25:58 +0700

    pynative exercise 2

--- python/pynativeBasic.py
+++ python/pynativeBasic.py
@@ -4,7 +4,7 @@
 # Exercise 1: Calculate the multiplication and sum of two numbers
 # Given two integer numbers, multiply their product only if the product is equal to or lower than 1000. Otherwise, return their sum.
 
-# Practice 1 & 2
+# Given 1 & 2
 print("1. Answer:")
 myTuple = ((20,30),(40,30),(30,25),(50,45))
 for contents in myTuple:
@@ -12,3 +12,13 @@ for contents in myTuple:
         print(contents[0],"x",contents[1],"=", contents[0] * contents[1])
     else:
         print(contents[0],"+",contents[1],"=", contents[0] + contents[1])
+
+# Exercise 2: Print the sum of the current number and the previous number
+# Write a program to iterate the first 10 numbers, and in each iteration, print the sum of the current and previous number.
+print("2. Answer:")
+for i in range(10):
+    j = i-1
+    if (j < 0):
+        print("Current Number",i,"Previous Number",i,"Sum:",i)
+    else:
+        print("Current Number",i,"Previous Number",j,"Sum:",i+j)
