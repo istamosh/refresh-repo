2ad4a91491212d85d3c85155ea005000ed8361a7
istamosh <63596969+istamosh@users.noreply.github.com>
2023-12-02 13:45:50 +0700

    binary search example

--- javascript/algorithm.js
+++ javascript/algorithm.js
@@ -1,1 +1,23 @@
 // Placeholder Binary Search Algo.
+let myList = [2, 12, 27, 34, 49, 51, 60, 76, 83, 88, 95, 101]
+
+binarySearch(myList, 95)
+
+function binarySearch(list, query) {
+    firstBar = 0
+    lastBar = list.length -1
+    found = false
+    
+    while (firstBar <= lastBar && !found){
+        middlePoint = Math.floor((firstBar + lastBar) / 2); middlePoint
+        // found = true; break
+        if (query == list[middlePoint]){
+            console.log(`found at index [${ middlePoint }]`)
+            found = true
+        }
+        else {
+            if (query < list[middlePoint]) lastBar = middlePoint - 1
+            else firstBar = middlePoint + 1
+        }
+    }
+}
