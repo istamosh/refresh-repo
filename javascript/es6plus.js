02fb68bc5c925caf3eb738991b74b13863097a1d
istamosh <63596969+istamosh@users.noreply.github.com>
2023-12-26 15:04:47 +0700

    JS: ES6 basics
    string, addition, newline alternative, accessing obj. and using alias, destructuring array, object literal with function

---
+++ javascript/es6plus.js
@@ -0,0 +1,42 @@
+// using string template literals
+let word1 = 'John';
+let word2 = 'Doe';
+const fullName = `${word1} ${word2}`;
+console.log(fullName);
+
+// addition
+let num1 = 5
+let num2 = 10
+const addition = `${num1 + num2}`;
+console.log(addition)
+
+// using string with newline (\n) alternative on both console and p-tag
+var nameWithNewline = `${word1}
+${word2}
+`
+console.log(nameWithNewline)
+document.getElementById('name-with-newline').innerText = nameWithNewline
+
+// accessing object info and using custom alias
+const personalInfo = {
+    firstName: 'Helen',
+    lastName: 'Berrington',
+    city: 'Austin',
+    state: 'Texas',
+    postalCode: 73302
+}
+const {lastName: ln, firstName: fn} = personalInfo
+console.log(`${ln}, ${fn}`)
+
+// destructuring array
+let [first, mid, last] = ['John', 'Rap Demigod', 'Castielle'];
+console.log(first + mid + last)
+last = 'Clementine'
+console.log(first + last)
+
+// object literal
+function addressMaker(city, state){
+    const fullAddress = {city, state}
+    console.log(fullAddress)
+}
+addressMaker('West Jakarta', 'Jakarta Region Special Capital')
