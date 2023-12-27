ee3bab9b9bf8ea7e07f39ef36deb457aa149f5d3
btiger-laptop <63596969+istamosh@users.noreply.github.com>
2023-12-27 21:10:46 +0700

    JS: ES6+ for loop, if else, iterable, spread, rest operator, nested function, reduce, and arrow function

--- javascript/es6plus.js
+++ javascript/es6plus.js
@@ -41,7 +41,7 @@ function addrMaker(city, state){
 }
 addrMaker('West Jakarta', 'Jakarta Region Special Capital')
 
-// object literal challenge
+// object literal challenge (with arrow function)
 const addressMaker = (address) => {
     const newAddress = {
         city: address.city,
@@ -52,3 +52,53 @@ const addressMaker = (address) => {
     console.log(`${ct}, ${st}, ${cy}`);
 }
 addressMaker({city:'Austin', state: 'Texas'})
+
+// for loop and iterable values
+let accountStatement = [752, 1008, 625, 690]
+scan(accountStatement, "calculate")
+let gigachad = "Ernest Khalimov the Gigachad"
+scan(gigachad);
+function scan(param, ops) {
+    if (ops == "calculate") {
+        total = 0;
+        for (const i of param) {
+            total += i;
+        }
+        console.log(total)
+    }
+    else {
+        for (const i of param) {
+            console.log(i);
+        }
+    }
+}
+
+// spread operator (copying entire array items)
+let array1 = [1,2,3,4,5,6,7]
+let array2 = [...array1]
+console.log(array2)
+array2.push(true);
+console.log(array2)
+
+// rest operator (rest several values as array)
+function restNums(...nums) {
+    console.log(nums)
+}
+restNums(10,2,5,9,1)
+
+// nested function, reduce array by 1 after being processed
+sumThese(10,2,5,9,1)
+function sumThese(...params) {
+    let total = params.reduce(function(x, y) {
+        return x + y;
+    })
+    console.log(total)
+}
+
+// arrow function variant (eliminates return, curly braces(if there have one statement))
+// arrow function can't be hoisted (must init. function first then call it)
+const sumThese2 = (...params) => {
+    let total = params.reduce((x, y) => x + y)
+    console.log(total)
+}
+sumThese2(10,2,5,9,1)
